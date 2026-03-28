f954c15059fc9a04b99c47fa90debbbc19bb3851
dhimaswara <supp.hashmicro@gmail.com>
2026-03-28 21:18:27 +0700

    debug: tampilkan family_id dan jumlah anggota di login anggota

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -243,7 +243,8 @@ const AuthPage = ({ onNavigate, onLoginSuccess, onFamilyLoginSuccess, familyMemb
                         </div>
                         <div>
                           <div style={{ fontWeight: 700, fontSize: '0.88rem' }}>{memberList.family.name}</div>
-                          <div style={{ fontSize: '0.7rem', color: '#a8a29e' }}>{memberList.members.length} anggota dengan PIN</div>
+                          <div style={{ fontSize: '0.7rem', color: '#a8a29e' }}>{memberList.members.length} anggota ditemukan</div>
+                          <div style={{ fontSize: '0.65rem', color: '#f59e0b', wordBreak: 'break-all' }}>family_id: {memberList.family.id}</div>
                         </div>
                         <button onClick={() => { setMemberList(null); setFamilyCode(''); setError(''); setSelectedMemberId(''); setPin(''); }} style={{ marginLeft: 'auto', background: 'none', border: 'none', cursor: 'pointer', color: '#a8a29e' }}>
                           <ChevronLeft size={16} />
